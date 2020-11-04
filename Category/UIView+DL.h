c9b2b07a84a313a2b71f5ac394281b9b11ecd952
kongdelu <kongdelu2009@yeah.net>
2020-11-04 12:50:21 +0800

    add UIView-DL

---
+++ Category/UIView+DL.h
@@ -0,0 +1,49 @@
+//
+//  UIView+DL.h
+//  Bhex
+//
+//  Created by ios on 2020/9/21.
+//  Copyright © 2020 Bhex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef NS_OPTIONS(NSUInteger, UIBorderSideType) {
+    UIBorderSideTypeAll  = 0,
+    UIBorderSideTypeTop = 1 << 0,
+    UIBorderSideTypeBottom = 1 << 1,
+    UIBorderSideTypeLeft = 1 << 2,
+    UIBorderSideTypeRight = 1 << 3,
+};
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (DL)
+
+/**
+ *  设置部分圆角(绝对布局)
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
+ */
+- (void)addRoundedCorners:(UIRectCorner)corners
+                withRadii:(CGSize)radii;
+/**
+ *  设置部分圆角(相对布局)
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
+ *  @param rect    需要设置的圆角view的rect
+ */
+- (void)addRoundedCorners:(UIRectCorner)corners
+                withRadii:(CGSize)radii
+                 viewRect:(CGRect)rect;
+
+/**
+ 给View添加指定位置的边框线
+ */
+- (UIView *)borderForColor:(UIColor *)color borderWidth:(CGFloat)borderWidth borderType:(UIBorderSideType)borderType;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Category/UIView+DL.m
@@ -0,0 +1,98 @@
+//
+//  UIView+DL.m
+//  KDL
+//
+//  Created by ios on 2020/9/21.
+//  Copyright © 2020 xxx. All rights reserved.
+//
+
+#import "UIView+DL.h"
+
+@implementation UIView(DL)
+
+/**
+ *  设置部分圆角(绝对布局)
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
+ */
+- (void)addRoundedCorners:(UIRectCorner)corners
+                withRadii:(CGSize)radii {
+    
+    UIBezierPath* rounded = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:corners cornerRadii:radii];
+    CAShapeLayer* shape = [[CAShapeLayer alloc] init];
+    [shape setPath:rounded.CGPath];
+    
+    self.layer.mask = shape;
+}
+
+/**
+ *  设置部分圆角(相对布局)
+ *
+ *  @param corners 需要设置为圆角的角 UIRectCornerTopLeft | UIRectCornerTopRight | UIRectCornerBottomLeft | UIRectCornerBottomRight | UIRectCornerAllCorners
+ *  @param radii   需要设置的圆角大小 例如 CGSizeMake(20.0f, 20.0f)
+ *  @param rect    需要设置的圆角view的rect
+ */
+- (void)addRoundedCorners:(UIRectCorner)corners
+                withRadii:(CGSize)radii
+                 viewRect:(CGRect)rect {
+    
+    UIBezierPath* rounded = [UIBezierPath bezierPathWithRoundedRect:rect byRoundingCorners:corners cornerRadii:radii];
+    CAShapeLayer* shape = [[CAShapeLayer alloc] init];
+    [shape setPath:rounded.CGPath];
+    
+    self.layer.mask = shape;
+}
+
+
+- (UIView *)borderForColor:(UIColor *)color borderWidth:(CGFloat)borderWidth borderType:(UIBorderSideType)borderType {
+    
+    if (borderType == UIBorderSideTypeAll) {
+        self.layer.borderWidth = borderWidth;
+        self.layer.borderColor = color.CGColor;
+        return self;
+    }
+    /// 左侧
+    if (borderType & UIBorderSideTypeLeft) {
+        /// 左侧线路径
+        [self.layer addSublayer:[self addLineOriginPoint:CGPointMake(0.f, 0.f) toPoint:CGPointMake(0.0f, self.frame.size.height) color:color borderWidth:borderWidth]];
+    }
+    
+    /// 右侧
+    if (borderType & UIBorderSideTypeRight) {
+        /// 右侧线路径
+        [self.layer addSublayer:[self addLineOriginPoint:CGPointMake(self.frame.size.width, 0.0f) toPoint:CGPointMake( self.frame.size.width, self.frame.size.height) color:color borderWidth:borderWidth]];
+    }
+    
+    /// top
+    if (borderType & UIBorderSideTypeTop) {
+        /// top线路径
+        [self.layer addSublayer:[self addLineOriginPoint:CGPointMake(0.0f, 0.0f) toPoint:CGPointMake(self.frame.size.width, 0.0f) color:color borderWidth:borderWidth]];
+    }
+    
+    /// bottom
+    if (borderType & UIBorderSideTypeBottom) {
+        /// bottom线路径
+        [self.layer addSublayer:[self addLineOriginPoint:CGPointMake(0.0f, self.frame.size.height) toPoint:CGPointMake( self.frame.size.width, self.frame.size.height) color:color borderWidth:borderWidth]];
+    }
+    
+    return self;
+}
+
+- (CAShapeLayer *)addLineOriginPoint:(CGPoint)p0 toPoint:(CGPoint)p1 color:(UIColor *)color borderWidth:(CGFloat)borderWidth {
+
+    /// 线的路径
+    UIBezierPath * bezierPath = [UIBezierPath bezierPath];
+    [bezierPath moveToPoint:p0];
+    [bezierPath addLineToPoint:p1];
+    
+    CAShapeLayer * shapeLayer = [CAShapeLayer layer];
+    shapeLayer.strokeColor = color.CGColor;
+    shapeLayer.fillColor  = [UIColor clearColor].CGColor;
+    /// 添加路径
+    shapeLayer.path = bezierPath.CGPath;
+    /// 线宽度
+    shapeLayer.lineWidth = borderWidth;
+    return shapeLayer;
+}
+@end
